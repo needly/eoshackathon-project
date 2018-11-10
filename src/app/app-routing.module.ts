cdb12dd332ae7ca7b19ef4fcc850aa743d265286
Tomas Bulva <megarameno@gmail.com>
2018-11-10 15:45:18 -0800

    added all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProjectComponent } from './project/project.component';
+import { ApplyComponent } from './apply/apply.component';
+import { ApplicationsComponent } from './applications/applications.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: '/profile', component: ProfileComponent },
+  { path: '/project', component: ProjectComponent },
+  { path: '/apply', component: ApplyComponent },
+  { path: '/applications', component: ApplicationsComponent }
 ];
 
 @NgModule({
