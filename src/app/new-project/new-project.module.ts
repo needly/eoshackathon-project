da1aceb5eaec17fe0b4aed345d30d2330e8638d4
Tomas Bulva <megarameno@gmail.com>
2018-11-11 07:51:29 -0800

    fixed some minor syntax errors

---
+++ src/app/new-project/new-project.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewProjectComponent } from './new-project.component';
+
+@NgModule({
+  declarations: [NewProjectComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NewProjectModule { }
